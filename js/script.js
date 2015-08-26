555c150ca687d6d7ed0c71a95746cd974c6aeb10
jinlee4140 <jinlee4140@gmail.com>
2015-08-26 00:20:53 -0400

    #next button

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
-	alert('javascript is working');
-})
+	// Set Options
+
+	var speed = 500;             //fade speed
+	var autoswitch = true;		 //auto-slider options
+	var autoswitch_speed = 4000; //auto-slider speed
+
+	//Add initial active class
+
+	$('.slide').first().addClass('active');
+
+	//Hide all slides
+	$('.slide').hide()
+
+	//Show first slide
+	$('.active').show();	
+
+	//
+	$('#next').on('click', function() {
+		alert('next was clicked');
+	})
+
+});
